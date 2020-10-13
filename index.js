360db0804b6717b7c2df8648dcf21c0588a0d452
Nirmalraj10567 <55886074+Nirmalraj10567@users.noreply.github.com>
2020-10-13 13:07:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 
-const { Composer } = require('micro-bot')
+const Telegraf = require('telegraf');
 
-const bot = new Composer()
+const API_TOKEN = process.env.API_TOKEN || '';
+
+const PORT = process.env.PORT || 3000;
+
+const URL = process.env.URL || 'https://your-heroku-app.herokuapp.com';
+
+const bot = new Telegraf(API_TOKEN);
+
+bot.telegram.setWebhook(`${URL}/bot${API_TOKEN}`);
+
+bot.startWebhook(`/bot${API_TOKEN}`, null, PORT)
+
+
 
 
   
