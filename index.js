08ad9f1fd6494d23286addbc8dcdfba37c966ce4
Nirmalraj10567 <55886074+Nirmalraj10567@users.noreply.github.com>
2020-11-13 17:59:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -54,7 +54,18 @@ const zx = axios.get('https://gplinks.in/api?api=1b1093a7f93afbe94ad2df18b89861d
 });
 
 bot.start()
-
+const TelegramBot = require('node-telegram-bot-api');
+const token = '1355780647:AAHvGWqbgZyxKZ-7ZjDj_YjL2vbRP4pV1e4';
+const bot = new TelegramBot(token, {polling: true});
+bot.onText(/\/gplinks (.+)/, (msg, match) => {
+  const chatId = msg.chat.id;
+  const resp = match[1]; // the captured "whatever"
+  const axios = require('axios')
+const zx = axios.get('https://gplinks.in/api?api=1b1093a7f93afbe94ad2df18b89861d4970c23b8&url='+resp).then((response) => {
+  A= response.data.shortenedUrl
+    return  bot.sendMessage(chatId, A);
+}) 
+});
 
 
 
