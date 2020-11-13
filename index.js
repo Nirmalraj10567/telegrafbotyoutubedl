06e736679a570e4c9cffa2cd44e81514019587ca
Nirmalraj10567 <55886074+Nirmalraj10567@users.noreply.github.com>
2020-11-13 15:31:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-
-
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+express().get('/', (req, res) => res.redirect('https://t.me/urlicupload'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 
 
